8dc1b742dca0b9d73e21fe567d99227ec6746b28
condurre <japeterson@extremenetworks.com>
2024-08-16 13:15:47 -0700

    hardcode my IPs for now

--- test/build.sh
+++ test/build.sh
@@ -10,6 +10,9 @@ python3 -m pip install --upgrade pip
 
 python3 -m pip install --upgrade -r requirements.txt
 
+export DEVICE_IP="10.0.20.136"
+export SERVER_IP="10.0.20.112"
+
 # env
 
 pytest -v -s .
